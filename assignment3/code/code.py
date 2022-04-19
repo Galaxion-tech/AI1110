4707b2326009f3faf2e3cf4d2280300c2aaf19ad
Galaxion-tech <92298338+Galaxion-tech@users.noreply.github.com>
2022-04-20 01:18:30 +0530

    Add files via upload

---
+++ assignment3/code/code.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+
+#input data
+data = pd.read_excel('./tables/mode.xlsx','Sheet1')
+mydata =np.array(data)
+smple_spce=20
+
+#possible number of marks (out of 10)
+marks=[i for i in range(12)]
+
+#number of student getting marks
+freq_stdnt=np.histogram(mydata,marks)
+freq_stdnt=np.asarray(freq_stdnt[0])
+
+#removing mark[11]
+marks.pop()
+
+#probablity distribution of data
+prob=freq_stdnt/smple_spce
+
+#getting which is recieved by max number of student
+mode= np.where(freq_stdnt==np.amax(freq_stdnt))
+mode= np.asarray(mode)[0][0]
+
+#printing
+print(mode)
+
+#writing in .xlsx
+write=pd.DataFrame({"Marks Obtained \n(out of 10)(X)":marks,"Frequency of\nStudent":freq_stdnt,"Probabilty Distribution \n(P(X=x))":prob})
+write.to_excel('tables/out.xlsx',index=False)
+
---
+++ assignment3/code/fig.py
@@ -0,0 +1,29 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import pandas as pd
+
+#opening file
+data = pd.read_excel('/home/harshsunny/Desktop/sem2/PeRV/assignment3/myassignment/tables/out.xlsx','Sheet1')
+
+marks=data.to_numpy()[:,0] #x coordinate
+prob=data.to_numpy()[:,2]  #y coordinate
+
+#plotting
+plt.plot(marks,prob, label="Probability Distribution")
+
+plt.ylim(0,1)
+plt.xlim(0,10)
+
+#labelling 
+plt.xlabel("Marks")
+plt.ylabel("Probability Distribution")
+
+plt.legend(loc="best")
+
+plt.grid()
+
+#saving figure
+plt.savefig("./figures/fig.png")
+
+#displaying
+plt.show()
