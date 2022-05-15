29a0ab180c39d95082dd87a32b8b8fbbee670f39
galaxion <92298338+Galaxion-tech@users.noreply.github.com>
2022-05-15 17:03:51 +0530

    Update code.py

--- assignment4/code/code.py
+++ assignment4/code/code.py
@@ -1,7 +1,3 @@
-import numpy as np
-from scipy.stats import bernoulli
-
-
 #Given data
 P_A1= 0.8
 P_B1= 0.7
@@ -10,4 +6,3 @@ P_AorB1=0.95
 #Inclusion and Exclusion
 P_AandB1=P_A1+P_B1-P_AorB1
 print(P_AandB1)
-
